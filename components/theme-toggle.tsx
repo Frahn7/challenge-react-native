cdcb34ece096a13fcdd2156f7a905ff7dc4ee23e
Francisco Villella <franciscovillella2003@gmail.com>
2025-10-21 16:53:12 -0300

    changes toggle

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,26 +1,20 @@
-import { Pressable, Text } from "react-native";
+import { Pressable } from "react-native";
 import { useAtom } from "jotai";
 import { themeAtom } from "@/features/themeAtom";
-import { useThemeColors } from "@/hooks/use-theme-colors";
 import { Ionicons } from "@expo/vector-icons";
 import { order } from "@/utils/types";
 
 export function ThemeToggle() {
   const [pref, setPref] = useAtom(themeAtom);
-  const { bg, text } = useThemeColors();
   return (
     <Pressable
       onPress={() => setPref(order[(order.indexOf(pref) + 1) % order.length])}
       style={{
         padding: 12,
-        backgroundColor: bg,
         alignItems: "center",
       }}
     >
-      <Text style={{ color: text }}>
-        {<Ionicons name="power-outline" size={24} color={text} />}
-      </Text>
-      <Text style={{ color: text, fontSize: 15 }}>{pref}</Text>
+      <Ionicons name="power-outline" size={24} color={"black"} />
     </Pressable>
   );
 }
