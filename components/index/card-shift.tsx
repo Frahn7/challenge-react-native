73dbb9b39ad7670e890ccb869229b6afad5cb89d
Frahn <franciscovillella2003@gmail.com>
2025-10-29 14:47:47 -0300

    Fix handleDelete

--- components/index/card-shift.tsx
+++ components/index/card-shift.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ThemedView } from "../themed-view";
 import { ThemedText } from "../themed-text";
 import { helperDate } from "../../lib/helper-fechas";
-import { TouchableOpacity, View } from "react-native";
+import { Alert, TouchableOpacity, View } from "react-native";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
 import { useDispatch } from "react-redux";
@@ -17,11 +17,12 @@ export const CardShift = ({ turno }: { turno: ShiftProps }) => {
 
   const handleDelete = async (id: number) => {
     try {
-      dispatch(eliminarTurno(id));
       await deleteShift(id);
+      dispatch(eliminarTurno(id));
       router.replace("/");
     } catch (e) {
       console.error(e);
+      Alert.alert("Hubo un error", "Intentalo de nuevo");
     }
   };
 
